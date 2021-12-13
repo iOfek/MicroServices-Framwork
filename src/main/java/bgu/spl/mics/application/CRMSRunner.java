d3d7cd6de2e2484f81ad7630734ef5087bc7e1b2
nadavdv <nadavdv@gmail.com>
2021-12-14 01:20:13 +0200

    Parsing and initializing microServices

--- src/main/java/bgu/spl/mics/application/CRMSRunner.java
+++ src/main/java/bgu/spl/mics/application/CRMSRunner.java
@@ -1,10 +1,15 @@
 package bgu.spl.mics.application;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javax.jws.WebParam.Mode;
 
+import bgu.spl.mics.application.services.*;
 import com.google.gson.Gson;
 
 import bgu.spl.mics.application.objects.CPU;
@@ -15,9 +20,7 @@ import bgu.spl.mics.application.messages.TrainModelEvent;
 import bgu.spl.mics.application.objects.*;
 import bgu.spl.mics.application.objects.GPU;
 import bgu.spl.mics.application.objects.Student;
-import bgu.spl.mics.application.services.GPUService;
-import bgu.spl.mics.application.services.StudentService;
-import bgu.spl.mics.application.services.TimeService;
+import com.google.gson.stream.JsonReader;
 
 /** This is the Main class of Compute Resources Management System application. You should parse the input file,
  * create the different instances of the objects, and run the system.
@@ -26,40 +29,99 @@ import bgu.spl.mics.application.services.TimeService;
 public class CRMSRunner {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Student[]students;
-        GPU[]gpus;
-        CPU[]cpus;
-        ConfrenceInformation[]confrences;
-        int tickTime;
-        int duration;
-        Data data = new Data(Data.Type.Text, 0, 1000000);
-        DataBatch dataBatch = new DataBatch(data, 0);
-        Model model1 = new Model("model1", data, Model.Status.PreTrained, Model.Result.None);
-        Model model2 = new Model("model2", data, Model.Status.PreTrained, Model.Result.None);
-        Model model3 = new Model("model3", data, Model.Status.PreTrained, Model.Result.None);
-        Model model4 = new Model("model4", data, Model.Status.PreTrained, Model.Result.None);
 
-        Model[] models1 = {model1,model2};
-        Student student1 = new Student("name", "CS", Student.Degree.MSc, 0, 0,models1);
-        Model []models2 ={model3,model4};
-        Student student2 = new Student("name", "CS", Student.Degree.MSc, 0, 0,models2);
-        GPU gpu = new GPU(GPU.Type.RTX3090);
-        Cluster cluster = Cluster.getInstance();
-        TimeService timeService = TimeService.getInstance();
-        StudentService studentService1 = new StudentService("student 1", student1);
-        StudentService studentService2 = new StudentService("student 2", student2);
-        GPUService gpuService = new GPUService("GPU Service", gpu);
+        Student[] students = null;
+        GPU[] gpus = null;
+        LinkedList<CPU> cpus = null;
+        ConfrenceInformation[] conferences = null;
+        int tickTime=0;
+        int duration=0;
+
+        String[] tempGPUs=null;
+        int[] tempCPUs=null;
+
+        try {
+            Gson gson = new Gson();
+            Reader reader = Files.newBufferedReader(Paths.get("example_input.json"));
+            JsonReader jReader = new JsonReader(reader);
+
+            jReader.beginObject();
+            while (jReader.hasNext()){
+                String name = jReader.nextName();
+               if (name.equals("Students")) {
+                   students = gson.fromJson(jReader,Student[].class);
+               }
+                if (name.equals("GPUS")) {
+                    tempGPUs = gson.fromJson(jReader,String[].class);
+                    gpus= new GPU[tempGPUs.length];
+                    for(int i=0;i<tempGPUs.length;i++){
+                        gpus[i]=new GPU(GPU.Type.valueOf(tempGPUs[i]));
+                    }
+                }
+                if (name.equals("CPUS")) {
+                    tempCPUs = gson.fromJson(jReader,int[].class);
+                    for(int i=0;i<tempCPUs.length;i++){
+                        cpus.add(new CPU(tempCPUs[i]));
+                    }
+                }
+                if (name.equals("Conferences")) {
+                    conferences = gson.fromJson(jReader,ConfrenceInformation[].class);
+                }
+                if (name.equals("TickTime")) {
+                    tickTime = jReader.nextInt();;
+                }
+                if (name.equals("Duration")) {
+                    duration = jReader.nextInt();;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
 
         ExecutorService e = Executors.newFixedThreadPool(3);
-        e.submit(gpuService);
-        try {
-            Thread.sleep(1000);
-        } catch (Exception ex) {
-            //TODO: handle exception
+        for(int i=0;i<gpus.length;i++) {
+            e.submit(new GPUService("GPU Service "+ i,gpus[i]));
         }
-        e.submit(timeService);
-        e.submit(studentService1);
-        e.submit(studentService2);
+        for(int i=0;i<cpus.size();i++) {
+            e.submit(new CPUService("CPU Service "+ i));
+        }
+        for(int i=0;i<conferences.length;i++) {
+            e.submit(new ConferenceService("Conference Service "+i,conferences[i]));
+        }
+        for(int i=0;i<students.length;i++) {
+            e.submit(new StudentService("Student Service "+i,students[i]));
+        }
+        e.submit(new TimeService(tickTime,duration));
+
+
+//        Student[]students;
+//        GPU[]gpus;
+//        CPU[]cpus;
+//        ConfrenceInformation[]confrences;
+//        int tickTime;
+//        int duration;
+//        Data data = new Data(Data.Type.Text, 0, 1000000);
+//        DataBatch dataBatch = new DataBatch(data, 0);
+//        Model model = new Model("testinggg", data, Model.Status.PreTrained, Model.Result.None);
+//        Model[] m = {model};
+//        Student student = new Student("name", "CS", Student.Degree.MSc, 0, 0,m);
+//
+//        GPU gpu = new GPU(GPU.Type.RTX3090, null);
+//        Cluster cluster = Cluster.getInstance();
+//        TimeService timeService = TimeService.getInstance();
+//        StudentService studentService = new StudentService("studen", student);
+//        GPUService gpuService = new GPUService("GPU Service", gpu);
+//
+//        ExecutorService e = Executors.newFixedThreadPool(3);
+//        e.submit(gpuService);
+//        try {
+//            Thread.sleep(1000);
+//        } catch (Exception ex) {
+//            //TODO: handle exception
+//        }
+//        e.submit(timeService);
+//        e.submit(studentService);
         
         
 
